f83e1d6c6e3b379ff84a3bd20e47d8727ee651d1
Hyma <sivarathrih@gmail.com>
2026-04-07 23:02:24 +0530

    Deploy

---
+++ script.js
@@ -0,0 +1,1 @@
+console.log('ResearchCollab static script loaded.');
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import AdminDashboard from "./pages/AdminDashboard";
 import ResearcherDashboard from "./pages/ResearcherDashboard";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/researcher" element={<ResearcherDashboard />} />
         <Route path="*" element={<Login />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
